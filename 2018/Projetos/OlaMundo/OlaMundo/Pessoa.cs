e1d6e909680a6076e594e509bbba0b200a5796c2
Prof. Julio Momente <prof.juliomomente@gmail.com>
2018-03-08 20:47:19 -0300

    conteudos de aula

---
+++ 2018/Projetos/OlaMundo/OlaMundo/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlaMundo
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+
+        public void saudar()
+        {
+            Console.WriteLine("Olá Mundo!");
+        }
+
+        public void apresentar()
+        {
+            Console.WriteLine("Olá, meu nome é " + Nome + ".");
+        }
+
+        public void obterInformacoes()
+        {
+            Console.Write("Informe seu nome: ");
+            Nome = Console.ReadLine();
+
+            Console.Write("Informe sua idade: ");
+            Idade = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
---
+++ 2018/Projetos/OlaMundo/OlaMundo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa P = new Pessoa();
+
+            
+
+            Console.WriteLine("Pressione qualquer tecla para continuar...");
+            Console.ReadKey();
+        }
+    }
+}
