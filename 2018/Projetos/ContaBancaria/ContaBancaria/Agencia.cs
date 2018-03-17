f7d3b1fd9850115aee9d927b677a27a9caf98d3d
Prof. Julio Momente <prof.juliomomente@gmail.com>
2018-03-16 21:24:36 -0300

    conteúdos de aula

---
+++ 2018/Projetos/ContaBancaria/ContaBancaria/Agencia.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    class Agencia
+    {
+        //Atributos
+        private string Banco;
+        private string Numero;
+        private string Endereco;
+        private string Telefone;
+        private string Gerente;
+
+        /// <summary>
+        /// Método construtor da classe agência
+        /// Faz a leitura dos valores dos atributos
+        /// </summary>
+        public Agencia()
+        {
+            Console.Write("Informe o banco: ");
+            Banco = Console.ReadLine();
+            Console.Write("Informe o número: ");
+            Numero = Console.ReadLine();
+            Console.Write("Informe o endereço: ");
+            Endereco = Console.ReadLine();
+            Console.Write("Informe o telefone: ");
+            Telefone = Console.ReadLine();
+            Console.Write("Informe o gerente: ");
+            Gerente = Console.ReadLine();
+        }
+
+        //Métodos acessores
+        public string getBanco()
+        {
+            return Banco.ToUpper();
+        }
+
+        public string getNumero()
+        {
+            return Numero;
+        }
+
+        public string getEndereco()
+        {
+            return Endereco;
+        }
+
+        public string getTelefone()
+        {
+            return Telefone;
+        }
+
+        public string getGerente()
+        {
+            return Gerente;
+        }
+
+        //Modificadores
+        public void setEndereco(string Endereco)
+        {
+            this.Endereco = Endereco;
+        }
+
+        public void setTelefone(string Telefone)
+        {
+            this.Telefone = Telefone;
+        }
+
+        public void setGerente(string Gerente)
+        {
+            this.Gerente = Gerente;
+        }
+
+        public void display()
+        {
+            Console.WriteLine(Banco);
+            Console.WriteLine("Agencia: " + Numero);
+            Console.WriteLine("Endereço: " + Endereco);
+            Console.WriteLine("Telefone: " + Telefone);
+            Console.WriteLine("Gerente: " + Gerente);
+        }
+    }
+}
---
+++ 2018/Projetos/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Agencia A = new Agencia();
+
+            Console.WriteLine(A.getBanco() + " - Ag.: " + A.getNumero());
+            Console.Write("Informe o novo telefone: ");
+            string novoTelefone = Console.ReadLine();
+            Console.Write("Informe o novo gerente: ");
+            string novoGerente = Console.ReadLine();
+
+            A.setTelefone(novoTelefone);
+            A.setGerente(novoGerente);
+
+            Console.WriteLine("\n\n");
+
+            A.display();
+
+            Console.WriteLine("\n\nPressione qualquer tecla para terminar ...");
+            Console.ReadKey();
+        }
+    }
+}
